c9644011487d25779f4996085b5d6456108275e6
Marcus macbook <marcusmac@Marcuss-MacBook-Air.local>
2026-05-15 14:49:58 -0500

    Add Section, Popup, PopupWithForm, and TodoCounter classes

---
+++ components/PopupWithForm.js
@@ -0,0 +1,33 @@
+import { Popup } from "./Popup.js";
+
+class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);  // calls Popup's constructor
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector(".popup__form");
+  }
+
+  _getInputValues() {
+    const inputs = this._form.querySelectorAll(".popup__input");
+    const values = {};
+    inputs.forEach((input) => {
+      values[input.name] = input.value;
+    });
+    return values;
+  }
+
+  close() {
+    super.close();        // calls Popup's close() method
+    this._form.reset();   // then does this extra step
+  }
+
+  setEventListeners() {
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+    super.setEventListeners();  // also runs Popup's setEventListeners
+  }
+}
+
+export { PopupWithForm };
--- pages/index.js
+++ pages/index.js
@@ -2,55 +2,61 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import { Todo } from "../components/Todo.js";
 import { FormValidator } from "../components/FormValidator.js";
+import { Section } from "../components/Section.js";
+import { PopupWithForm } from "../components/PopupWithForm.js";
+import { TodoCounter } from "../components/TodoCounter.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
-const addTodoPopup = document.querySelector("#add-todo-popup");
-const addTodoForm = addTodoPopup.querySelector(".popup__form");
-const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-const todosList = document.querySelector(".todos__list");
 
-const openModal = (modal) => {
-  modal.classList.add("popup_visible");
-};
-
-const closeModal = (modal) => {
-  modal.classList.remove("popup_visible");
-};
+const todoCounter = new TodoCounter(initialTodos, ".counter__text");
 
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
-  return todo.getView();
+  return todo.getView({
+    handleDelete: (completed) => {
+      if (completed) {
+        todoCounter.updateCompleted(false);
+      }
+      todoCounter.updateTotal(false);
+    },
+    handleCheck: (isCompleted) => {
+      todoCounter.updateCompleted(isCompleted);
+    },
+  });
 };
 
 const renderTodo = (data) => {
   const todo = generateTodo(data);
-  todosList.append(todo);
+  todoSection.addItem(todo);
 };
 
-const formValidator = new FormValidator(validationConfig, addTodoForm);
-formValidator.enableValidation();
-
-addTodoButton.addEventListener("click", () => {
-  openModal(addTodoPopup);
+const todoSection = new Section({
+  items: initialTodos,
+  renderer: renderTodo,
+  containerSelector: ".todos__list",
 });
 
-addTodoCloseBtn.addEventListener("click", () => {
-  closeModal(addTodoPopup);
-});
+todoSection.renderItems();
 
-addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const name = evt.target.name.value;
-  const dateInput = evt.target.date.value;
+const addTodoPopup = new PopupWithForm("#add-todo-popup", (data) => {
+  const name = data.name;
+  const dateInput = data.date;
 
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
   renderTodo({ name, date, id: uuidv4() });
+  todoCounter.updateTotal(true);  // 👈 added
   formValidator.resetValidation();
-  closeModal(addTodoPopup);
+  addTodoPopup.close();
 });
 
-initialTodos.forEach((item) => {
-  renderTodo(item);
+addTodoPopup.setEventListeners();
+
+const addTodoForm = document.querySelector("#add-todo-popup .popup__form");
+const formValidator = new FormValidator(validationConfig, addTodoForm);
+formValidator.enableValidation();
+
+addTodoButton.addEventListener("click", () => {
+  addTodoPopup.open();
 });
